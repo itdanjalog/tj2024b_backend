39d6efcfbc78e2fc40fdc488b41d068413f39086
itdanjalog <kgs2072@naver.com>
2024-12-10 16:27:27 +0900

    day22

---
+++ tj2024b_backend/src/day02/Example5.java
@@ -0,0 +1,35 @@
+package day02;
+
+public class Example5 { // class start 
+
+	public static void main(String[] args) { // main start 
+		
+		// 자바 과제1 : JS day10 연산자 문제 8개 (1_연산자.js) 를 자바로 풀이하기 
+		// 제출 방법 : 강의 카카오톡방에 풀이한 코드가 위치한 상세 git 주소 제출 
+		
+		// 입력 방법 : prompt() 대신에 Scanner scan = new Scanner(System.in);  scan.nextXXX();
+		// 출력 방법 : console.log() 대신에 System.out.println();
+		
+		// [지문1] 국어 , 영어 , 수학 점수를 각 입력받아서 각 변수에 저장하고 총점 과 평균을 계산하여 console탭에 출력하시오.
+		
+		// [지문2] 반지름를 입력받아서 원넓이[반지름*반지름*3.14] 계산하여 console탭에 출력하시오.
+		
+		// [지문3] 두 실수를 입력받아서 앞 실수의 값이 뒤실수의 값의 비율% 계산하여 console탭에 출력하시오.
+		
+		// [지문4] 정수를 입력받아 입력받은 값이 홀수이면 true / 짝수이면 false 로 console탭에 출력하시오.
+		
+		// [지문5] 정수를 입력받아 입력받은 값이 7의 배수이면 true / 아니면 false 로 console탭에 출력하시오.
+
+		// [지문6] 정수를 입력받아 입력받은 값이 홀수 이면서 7배수 이면 true / 아니면 false 로 console탭에 출력하시오.
+		
+		// [지문7] 십만원 단위의 금액을 입력받아 입력받은 금액의 지폐수를 console탭에 출력하시오.
+	    	// -> 입력 예] 356789 입력시 , 출력 예] 십만원:3장 만원:5장 천원6장 
+		
+		// [지문8] 1차점수 와 2차점수 각각 입력받아서 
+			// -> 총점이 150점이상이면 '합격' 아니면 '불합격' 으로 console탭에 출력하시오.
+		
+		
+
+	} // main end 
+
+} // class end 
