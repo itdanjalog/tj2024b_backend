816a02c551363c9d0395daed35562a41b13a3744
itdanjalog <kgs2072@naver.com>
2024-12-24 12:37:51 +0900

    day32

--- tj2024b_backend/src/day12/boardprogram7/MemberService.java
+++ tj2024b_backend/src/day12/boardprogram7/MemberService.java
@@ -9,7 +9,7 @@ public class MemberService {
 		System.out.print("비밀번호 : ");	String pwd = scan.next();
 		System.out.print("닉네임 : ");	String name = scan.next();
 		// * 객체화 . 생성자 이용한 빠른 객체 생성 
-		MemberDto memberDto = new MemberDto();
+		MemberDto memberDto = new MemberDto( id , pwd , name );
 		// * 반복활용한 회원목록에서 빈공간 찾기 
 		for( int index = 0 ; index <= members.length - 1 ; index++ ) {
 			if( members[index] == null ) {
@@ -26,6 +26,7 @@ public class MemberService {
 		// * 기존회원목록에서 방금 입력받은 값과 비교하여 동일값 찾기
 		for( int index = 0 ; index <= members.length -1 ; index++ ) {
 			MemberDto memberDto = members[index];
+			System.out.println( memberDto );
 			if( memberDto != null ) {
 				if( memberDto.getId().equals( id ) && memberDto.getPwd().equals(pwd) ) {
 					System.out.println("로그인 성공");
