2f3336f5625bb94a5d37decd2bb4a7466e69d218
itdanjalog <kgs2072@naver.com>
2025-01-07 13:26:00 +0900

    day40

--- tj2024b_backend/src/boardservice10/view/MemberView.java
+++ tj2024b_backend/src/boardservice10/view/MemberView.java
@@ -1,5 +1,10 @@
 package boardservice10.view;
 
 public class MemberView {
-
-}
+	
+	// + 싱글톤 
+	private static MemberView instance = new MemberView();
+	private MemberView() {}
+	public static MemberView getInstance() { return instance;} 
+	
+} // class ned 
