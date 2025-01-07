ac2e398da115602c9a18f3ca6c1d9e86fb64e49e
itdanjalog <kgs2072@naver.com>
2025-01-07 16:39:36 +0900

    day40

--- tj2024b_backend/src/boardservice10/controller/MemberController.java
+++ tj2024b_backend/src/boardservice10/controller/MemberController.java
@@ -1,8 +1,46 @@
 package boardservice10.controller;
 
+import boardservice10.model.dao.MemberDao;
+import boardservice10.model.dto.MemberDto;
+
 public class MemberController {
 	// + 싱글톤 
 	private static MemberController instance = new MemberController();
 	private MemberController() {}
 	public static MemberController getInstance() { return instance;} 
-}
+	
+	// 1. 회원가입 컨트롤러 메소드 
+	public boolean signup( MemberDto memberDto ) {
+		// * 다양한 유효성검사를 넣을 예정 
+		boolean result = MemberDao.getInstance().signup( memberDto );
+		return result;
+	}
+	
+} // class end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tj2024b_backend/src/boardservice10/model/dao/MemberDao.java
+++ tj2024b_backend/src/boardservice10/model/dao/MemberDao.java
@@ -2,6 +2,10 @@ package boardservice10.model.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import boardservice10.model.dto.MemberDto;
 
 public class MemberDao {
 	
@@ -24,7 +28,38 @@ public class MemberDao {
 	}
 	public static MemberDao getInstance() { return instance;} 
 	
-}
+	// 1. 회원가입 SQL 처리 메소드 
+	public boolean signup( MemberDto memberDto ) {
+		try {
+			// [1] SQL 작성한다.
+			String sql ="insert into member( mid , mpwd , mname , mphone ) "
+					+ "values( '"+memberDto.getMid()+"' , '"+memberDto.getMpwd()+"', '"+memberDto.getMname()+"', '"+memberDto.getMphone()+"' )";
+			// [2] DB와 연동된 곳에 SQL 기재한다. 		- 연동된 db에 sql 기재하는 방법 : conn.prepareStatement( SQL )
+			PreparedStatement ps = conn.prepareStatement(sql);
+			// [3] 기재된 SQL를 실행하고 결과를 받는다. . 	- 기재된 sql를 실행하는 방법 : ps.executeUpdate()
+			int count = ps.executeUpdate();
+			// [4] 결과에 따른 처리 및 반환를 한다.
+			if( count == 1 ) { return true; }
+		}catch( SQLException e ) { System.out.println( e ); }
+		return false;
+	} // f end 
+	
+} // class end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
