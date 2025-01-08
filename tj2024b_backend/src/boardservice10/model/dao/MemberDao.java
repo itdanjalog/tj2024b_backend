be71915f1c25115da550dc9a0ebe0bf6a5c8f3e3
itdanjalog <kgs2072@naver.com>
2025-01-08 13:25:04 +0900

    day41

--- tj2024b_backend/src/boardservice10/model/dao/MemberDao.java
+++ tj2024b_backend/src/boardservice10/model/dao/MemberDao.java
@@ -81,6 +81,27 @@ public class MemberDao {
 		return null;
 	} // f end 
 	
+	// 2. 로그인 SQL 처리 메소드
+	public int login( MemberDto memberDto ) {
+		// int : SQL로 조회된 회원번호를 반환하기 위해서
+		try {
+			// [1] SQL 작성한다.
+			String sql = "select mno from member where mid = ? and mpwd = ? ";
+			// [2] DB와 연동된 곳에 SQL 기재한다.
+			PreparedStatement ps =  conn.prepareStatement(sql);
+			// [*] 기재된 SQL 에 매개변수 값 대입한다.
+			ps.setString( 1 , memberDto.getMid() );
+			ps.setString( 2 , memberDto.getMpwd() );
+			// [3] 기재된 SQL 실행하고 결과를 받는다.
+			ResultSet rs = ps.executeQuery();
+			// [4] 결과에 따른 처리 및 반환를 한다.
+			if( rs.next() ) {
+				int mno = rs.getInt("mno");
+				return mno;
+			}
+		}catch( SQLException e ) { System.out.println( e ); }
+		return 0;
+	} // f end 
 } // class end 
 
 
